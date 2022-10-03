ead233f77747f6527950ce625e4fa3d4367c1505
Carlos Dutra <ch-dutra@hotmail.com>
2022-10-02 23:44:41 -0400

    Added cors to index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
 const { port } = require("./config");
 const express = require("express");
 const useCors = require("./middlewares/cors");
+
+const app = express();
+
+// Use JSON
+app.use(express.json());
+
+// CORS
+useCors(app);
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
